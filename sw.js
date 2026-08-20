f84e7596bdbf99d5e381927b155a6ef5defa910a
jdh29 <jameshiggs29@gmail.com>
2026-08-20 21:20:21 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,6 +7,8 @@ const ASSETS = [
   "/icon-192.png",
   "/icon-512.png",
   "/icon-180.png",
+  "/bookend-l.png",
+  "/bookend-r.png",
   "https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.5/babel.min.js",
